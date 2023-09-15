9fe596dae1a8a8476b9481fe948a5a8528700b3c
dombatersesat <benedkudat@gmail.com>
2023-09-15 10:35:02 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let menu =document.querySelector("#menu-icon");
+let navbar=document.querySelector(".navbar");
+ 
+menu.addEventListener("click", function(){
+     navbar.classList.toggle("active")
+});
